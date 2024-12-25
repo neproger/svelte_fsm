fc446f7f8854a5ac7295657d1a98dc32048e07ba
neproger <kulbaev666@fmail.com>
2024-12-25 15:47:11 +0500

    +

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'dist',     // Указывает, куда помещать HTML-файлы
+			assets: 'dist',    // Указывает, куда помещать статические файлы
+			fallback: '200.html'
+		}),
+		paths: {
+			base: '', // Убедитесь, что базовый путь пуст
+		},
+		prerender: {
+			entries: [] // Запрещаем предварительный рендеринг
+		}
 	}
 };
 
